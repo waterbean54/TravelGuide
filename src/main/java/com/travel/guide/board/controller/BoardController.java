8dfbcf15e7554e5645ecace6af45af1bb7a7f861
LEE HS <histar48@daum.net>
2020-04-14 20:23:55 +0900

    properties file 추가

--- src/main/java/com/travel/guide/board/controller/BoardController.java
+++ src/main/java/com/travel/guide/board/controller/BoardController.java
@@ -1,4 +1,9 @@
 package com.travel.guide.board.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
 public class BoardController {
 }
--- src/main/java/com/travel/guide/board/service/BoardService.java
+++ src/main/java/com/travel/guide/board/service/BoardService.java
@@ -1,4 +1,9 @@
 package com.travel.guide.board.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class BoardService {
 }
