cc9abbdefc6c9ede7f89d656213335cea30fb80f
waterbean54 <waterbean54@gmail.com>
2020-04-27 22:08:44 +0900

    기본

--- src/main/java/com/travel/guide/board/controller/BoardController.java
+++ src/main/java/com/travel/guide/board/controller/BoardController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BoardController {
 
-    @RequestMapping("/hello")
-    public String home(){
-        return "hello";
+    @RequestMapping("/board")
+    public String board(){
+        return "board/board";
     }
 }
